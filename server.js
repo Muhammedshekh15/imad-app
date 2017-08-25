7ab4575f12cb3158512b9ad6fb3719ac80457a85
Muhammedshekh <30692552+Muhammedshekh15@users.noreply.github.com>
2017-08-26 00:06:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res) {
-  res.send(createTemplete(articleOne)); 
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res) {
