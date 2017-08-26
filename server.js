9bccad03138377c2cd58af991d072dc577064fb2
Muhammedshekh <30692552+Muhammedshekh15@users.noreply.github.com>
2017-08-26 12:39:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,36 +7,39 @@ app.use(morgan('combined'));
 
 
 var articles = {
-   'article-one': {
-  title:"Article one I Shaikh Muhammed",
-  heading:" Article one",
-  date:" Aug 17,2017",
-  content:`   
-       <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-       </p>
-       <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-       </p>
-       <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-       </p>`
+    'article-one': {
+        title:"Article one I Shaikh Muhammed",
+        heading:" Article one",
+        date:" Aug 17,2017",
+        content:`   
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>`
 },
-   'article-two': {title:"Article two I Shaikh Muhammed",
-  heading:" Article two",
-  date:" Aug 20,2017",
-  content:`   
-       <p>
-           This is the content for my second article.
-       </p>`
+   'article-two': {
+        title:"Article two I Shaikh Muhammed",
+        heading:" Article two",
+        date:" Aug 20,2017",
+        content:`   
+            <p>
+               This is the content for my second article.
+            </p>`
 },
-   'article-three': { title:"Article Three I Shaikh Muhammed",
-  heading:" Article Three",
-  date:" Aug 25,2017",
-  content:`   
-       <p>
+'article-three': {
+    title:"Article Three I Shaikh Muhammed",
+    heading:" Article Three",
+    date:" Aug 25,2017",
+    content:`   
+        <p>
            This is the content for my third article.
-       </p>`}
+        </p>`
+    }
 };
 function createTemplete (data) {
 var title = data.title;
