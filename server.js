79dc2ee45b1039846b0122bd9561c9328b7cc458
Muhammedshekh <30692552+Muhammedshekh15@users.noreply.github.com>
2017-08-29 23:58:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,12 +42,12 @@ var articles = {
     }
 };
 function createTemplate (data) {
-       var title = data.title;
-       var date = data.date;
-       var heading = data.heading;
-       var content = data.content;
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
 
-       var htmlTemplate = `
+    var htmlTemplate = `
         <html>
             <head>
                 <title>
