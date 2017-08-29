d6fcde38e397b3f1fbc20c891c1b47c82ed48a64
Muhammedshekh <30692552+Muhammedshekh15@users.noreply.github.com>
2017-08-30 00:53:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/:article-one',function(req,res) {
 });
 
 app.get('/:article-two',function(req,res) {
-    res.send(createTemplate(articletwo));
+    res.send(createTemplate(articleTwo));
 });
 
 app.get('/:article-three',function(req,res) {
